f12b95b8ea18daa77c46a0a3635655d2968beec0
jqueijo- <jqueijo-@pop-os.localdomain>
2024-11-26 01:02:52 +0000

    initial commit

--- cub3d/includes/cub3d.h
+++ cub3d/includes/cub3d.h
@@ -6,7 +6,36 @@
 /*   By: jqueijo- <jqueijo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 19:17:42 by jqueijo-          #+#    #+#             */
-/*   Updated: 2024/11/20 19:17:43 by jqueijo-         ###   ########.fr       */
+/*   Updated: 2024/11/26 01:01:06 by jqueijo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CUB3D_h
+#define CUB3d_H
+
+# include "minilibx-linux/mlx.h"
+# include <stdint.h> // Define integer types, limits, macros
+# include <stdlib.h> // Memory allocation
+# include <stdio.h> // Standard input and output, perror function / Debugging
+# include <math.h> // math functions
+# include <unistd.h> // System calls
+# include <X11/keysym.h> // Predefined key symbols corresponding to keycodes
+# include <X11/X.h> // Constant definitions for Xlib functions
+
+#define ERROR_MESSAGE "Please enter:\n\n\
+'./cub3d $(MAP_NAME)\n\
+Press awsd to move and arrows to look around\n"
+
+# define WIDTH 800
+# define HEIGHT 800
+
+typedef struct s_data
+{
+	char	*path_to_map;
+	int		floor_color;
+	int		ceiling_color;
+	char	*path_to_textures;
+	char	*player;
+} t_data;
+
+#endif
