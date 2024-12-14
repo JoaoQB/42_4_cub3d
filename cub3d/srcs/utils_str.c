1d5f09156004f049cd0cdeb73cfb42e5491e71f1
juka <rather not>
2024-12-14 10:43:09 +0000

    unexistent files added

---
+++ cub3d/srcs/utils_str.c
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_str.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fandre-b <fandre-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/11 15:27:11 by fandre-b          #+#    #+#             */
+/*   Updated: 2024/12/11 15:31:08 by fandre-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void ft_putstr_fd(char *str, int fd)
+{
+	int ret;
+	if (!str)
+		return ;
+	ret = write(fd, str, ft_strlen(str));
+	(void)ret;
+}
+
+
+int	ft_wordcount(const char *str, char c)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		while (str[i] && c == str[i])
+			i++;
+		if (str[i] && str[i] != c)
+		{
+			count++;
+			while (str[i] && str[i] != c)
+				i++;
+		}
+	}
+	return (count);
+}
+
+int ft_strlen(const char *str)
+{
+	int i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str && str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strcat(char *dest, const char *src)
+{
+	char	*dest_end;
+
+	dest_end = dest;
+	while (*dest_end != '\0')
+		dest_end++;
+	while (*src != '\0')
+	{
+		*dest_end = *src;
+		dest_end++;
+		src++;
+	}
+	*dest_end = '\0';
+	return (dest);
+}
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	count;
+
+	if (!dest || !src)
+		return (0);
+	if (size == 0)
+		return (ft_strlen(src));
+	count = 0;
+	while (src[count] != '\0' && count < (size - 1))
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	dest[count] = '\0';
+	return (ft_strlen(src));
+}
+
+char* ft_strchr(char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	while ((*s1 && *s2) && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+    return (unsigned char)*s1 - (unsigned char)*s2;
+}
+
+int ft_startswith(const char *s1, const char *s2) {
+    int i;
+
+	i = 0;
+	while (s1[i] && s2[i]) 
+	{
+        if (s1[i] != s2[i])
+            break;
+		i++;
+    }
+	if (s2[i] == '\0')
+		return 0;
+    return 1;
+}
