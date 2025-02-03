ce513ad0c8eb7d90265c5bd575ac1207930780d6
fandre-b <fandre-b@student.42.fr>
2025-02-03 19:57:09 +0000

    door finalized

--- cub3d/srcs/mlx_utils/mlx_door.c
+++ cub3d/srcs/mlx_utils/mlx_door.c
@@ -6,7 +6,7 @@
 /*   By: fandre-b <fandre-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 15:05:53 by jqueijo-          #+#    #+#             */
-/*   Updated: 2025/02/03 19:54:09 by fandre-b         ###   ########.fr       */
+/*   Updated: 2025/02/03 19:56:56 by fandre-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,19 @@ void	door_switch(t_game *game)
 {
 	int			x;
 	int			y;
+	int			dist;
 	static bool	toggle;
 
 	toggle = !toggle;
 	if (!toggle)
 		return ;
-	game->ray.id = (WIDTH / 2) -1 ;
+	game->ray.id = (WIDTH / 2) -1;
 	cast_ray(game, &game->ray, "DC1");
 	x = game->ray.grid_x;
 	y = game->ray.grid_y;
+	dist = (int)game->ray.walls[game->ray.id].ray_dist;
+	if (dist < COLISION || dist < 1)
+		return ;
 	if (is_valid(y, x, "D"))
 		game->map[y][x] = 'C';
 	else if (is_valid(y, x, "C"))
