ad2cf10c75b3333f8506fe5deb72f3ff06a131fc
Filipe Andre Barata Antunes <fandre-b@c2r6s9.42lisboa.com>
2025-02-06 17:31:38 +0000

    save

--- cub3d/srcs/main.c
+++ cub3d/srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jqueijo- <jqueijo-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fandre-b <fandre-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 12:18:52 by jqueijo-          #+#    #+#             */
-/*   Updated: 2025/02/06 15:36:24 by jqueijo-         ###   ########.fr       */
+/*   Updated: 2025/02/06 17:31:29 by fandre-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,47 @@ void	init_game(char *file_path)
 	init_texture(ft_game());
 	init_ray(ft_game());
 	render();
+}
+
+int nft_wordcount(char *str, char c)
+{
+	int words;
+	
+	words = 0;
+	while (*str)
+	{
+		// printf("words %s\n", str);
+		while (*str && *str == c)
+			str++;
+		while (*str && *str != c)
+			str++;
+		words++;
+	}
+	return words;
+}
+
+char **ft_nsplit(char *str, char c)
+{
+	char	**map;
+	int		i;
+	int		j;
+
+	map = (char **) my_calloc(nft_wordcount(str, c) + 1, sizeof(char *));
+	j = -1;
+	while (*str)
+	{
+		i = 0;
+		while (str[i] && str[i] == c)
+			i++;
+		while (str[i] && str[i] != c)
+			i++;
+		map[++j] = ft_strnjoin(NULL, str, i);
+		str += i;
+		if (*str && *str == c)
+			str++;
+		printf("string: %s\n", map[j]);
+	}
+	return (map);
 }
 
 int	main(int argc, char **argv)
